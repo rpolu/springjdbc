48e7c443abe48e3379b221a32b8cb27a1dd4f33b
rpolu <rpolu@192.168.0.102>
2020-04-27 18:21:03 +0530

    commited

--- JDBC_Callable_Example/src/main/java/com/elegant/FuntionExample.java
+++ JDBC_Callable_Example/src/main/java/com/elegant/FuntionExample.java
@@ -1,26 +1,8 @@
 package com.elegant;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Types;
-
 public class FuntionExample {
 
 	public static void main(String[] args) throws Exception {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
-		String query = "{? =call manpulate(?,?)}";
-		CallableStatement cst = con.prepareCall(query);
-		cst.registerOutParameter(1, Types.INTEGER);
-		cst.setInt(2, 10);
-		cst.setInt(3, 20);
-		cst.execute();
-		System.out.println(cst.getInt(1));
-		System.out.println("Function calles succe fully");
-		cst.close();
-		con.close();
-
 	}
 
 }
--- JDBC_Callable_Example/src/main/java/com/elegant/StoredProcedureExample.java
+++ JDBC_Callable_Example/src/main/java/com/elegant/StoredProcedureExample.java
@@ -7,17 +7,7 @@ import java.sql.DriverManager;
 public class StoredProcedureExample {
 
 	public static void main(String[] args) throws Exception {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "tiger");
-		String query = "{ call insertData(?,?,?)}";
-		CallableStatement cst = con.prepareCall(query);
-		cst.setInt(1, 101);
-		cst.setString(2, "ram");
-		cst.setFloat(3, 899);
-		cst.execute();
-		System.out.println("Procedure calles succe fully");
-		cst.close();
-		con.close();
+		
 
 	}
 
