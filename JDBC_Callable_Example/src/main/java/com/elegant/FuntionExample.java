67427b55bbb01003d3e29474f8507709fe16a106
rpolu <rpolu@172.20.10.2>
2020-04-28 08:04:58 +0530

    Committed

--- JDBC_Callable_Example/src/main/java/com/elegant/FuntionExample.java
+++ JDBC_Callable_Example/src/main/java/com/elegant/FuntionExample.java
@@ -1,8 +1,31 @@
 package com.elegant;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Types;
+
 public class FuntionExample {
+	private static final String DRIVERCLASSNAME = "oracle.jdbc.driver.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String USERNAME = "system";
+	private static final String PASSWORD = "tiger";
 
 	public static void main(String[] args) throws Exception {
+
+		Class.forName(DRIVERCLASSNAME);
+		Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		CallableStatement cst = con.prepareCall("{ ?=call manpulate(?,?)}");
+		cst.registerOutParameter(1, Types.INTEGER);
+		cst.setInt(2, 10);
+		cst.setInt(3, 15);
+		cst.execute();
+
+		int sum = cst.getInt(1);
+		System.out.println(sum);
+		
+		cst.close();
+		con.close();
 	}
 
 }
--- JDBC_Callable_Example/src/main/java/com/elegant/StoredProcedureExample.java
+++ JDBC_Callable_Example/src/main/java/com/elegant/StoredProcedureExample.java
@@ -5,10 +5,33 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class StoredProcedureExample {
+	private static final String DRIVERCLASSNAME = "oracle.jdbc.driver.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String USERNAME = "system";
+	private static final String PASSWORD = "tiger";
 
-	public static void main(String[] args) throws Exception {
-		
-
+	public static void main(String[] args) {
+		Connection con = null;
+		CallableStatement cst = null;
+		try {
+			Class.forName(DRIVERCLASSNAME);
+			con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			cst = con.prepareCall("{ call insertData(?,?,?)}");
+			cst.setInt(1, 7777);
+			cst.setString(2, "Call_PROCEDURE");
+			cst.setFloat(3, 6666.90f);
+			cst.execute();
+			System.out.println("Procedure Invoked.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				cst.close();
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
